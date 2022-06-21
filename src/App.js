3ffd4b03d2a60fad831a1c3e4f5d47bf2c599dd0
vsmith95 <vaughnsmith95@gmail.com>
2022-06-21 11:22:06 -0400

    set up component file structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
-import Display from "./components/Display";
+import React, {useState} from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+import "./App.css"
 
 function App() {
-  return <Display />;
+  
 }
 
 export default App;
