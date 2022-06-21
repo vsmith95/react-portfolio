f7113a9a0c2824dde45fd52bf0f6eeeb39cad17f
vsmith95 <vaughnsmith95@gmail.com>
2022-06-21 11:30:18 -0400

    set up component index.js basics

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+    return (
+        <section>
+            <h1 id="about">
+                About me
+            </h1>
+        </section>
+    );
+    };
+
+    export default About;
