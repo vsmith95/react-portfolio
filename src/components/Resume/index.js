6f5f914e239f7de6395f7514e760a042dd99a5ef
vsmith95 <vaughnsmith95@gmail.com>
2022-06-25 21:04:21 -0400

    added note to resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -8,6 +8,9 @@ const Resume = () => {
                 <h3 className="section-title">
                     Resume Coming Soon
                 </h3>
+                <p className="">
+                    If you'd like to be featured on future updates to my resume please contact me at vaughnsmith95@gmail.com or call at (860)244-3569! Thank you and I look forward to hearing from you!
+                </p>
             </div>
         </section>
     )
