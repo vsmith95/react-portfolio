3929318f05b8277f0b1a35f7e1277b35cd4a5dac
vsmith95 <vaughnsmith95@gmail.com>
2022-06-25 21:27:39 -0400

    updated some links and stuff

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,8 +11,8 @@ const Footer = (props) => {
       return (
         <footer className="flex-row">
           <div className="contact-info">
-            <a href="">GitHub</a>
-            <a href="">Linkedin</a>
+            <a href="https://github.com/vsmith95">GitHub</a>
+            <a href="https://www.linkedin.com/in/vaughn-smith-256552157/">Linkedin</a>
             <a href="mailto: vaughnsmith95@gmail.com">vaughnsmith95@gmail</a>
             <a href="tel:8602443569">(860)244-3569</a>
           </div>  
