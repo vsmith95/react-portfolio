dd0f193f5d7b661bffdc87ca392d5e911412221d
vsmith95 <vaughnsmith95@gmail.com>
2022-08-18 04:00:19 -0400

    meta test again

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,7 +6,7 @@ const Resume = () => {
         <section>
             <div className="titleContainer">
                 <h3 className="section-title">
-                    <a className="Link" href="https://drive.google.com/file/d/14ttG7XHKcIO2rp5hYbzEGd5rSfU9zRFq/view?usp=sharing" target="_blank">Resume</a>
+                    <a className="Link" href="https://drive.google.com/file/d/14ttG7XHKcIO2rp5hYbzEGd5rSfU9zRFq/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
                 </h3>
                 <p className="resume-text">
                     If you'd like to be featured on future updates to my resume please contact me at vaughnsmith95@gmail.com or call at (860)244-3569! Thank you and I look forward to hearing from you!
